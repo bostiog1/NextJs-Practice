b8d1652e2b6d7975d2df4af1c496abd3939ddbcb
Teo <teo.bostiog@yahoo.com>
2024-09-29 15:34:30 +0300

    handling get,post,patch,delete + dynamic route + url query params + redirect route

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,20 +1,20 @@
 "use client";
 import { notFound } from "next/navigation";
 
-function getRandomInt(count: number) {
-  return Math.floor(Math.random() * count);
-}
+// function getRandomInt(count: number) {
+//   return Math.floor(Math.random() * count);
+// }
 
 export default function ReviewDetails({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
-  const random = getRandomInt(2);
+  // const random = getRandomInt(2);
 
-  if (random === 1) {
-    throw new Error("Error loading review");
-  }
+  // if (random === 1) {
+  //   throw new Error("Error loading review");
+  // }
 
   if (parseInt(params.reviewId) > 1000) {
     notFound();
