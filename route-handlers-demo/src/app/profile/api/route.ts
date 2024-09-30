70ed67bc73948044a98ffce3f71fad18f6d3616e
Teo <teo.bostiog@yahoo.com>
2024-09-30 15:44:53 +0300

    setup not working

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,21 @@
-export async function GET() {
-  return new Response("Profile API data");
+import { headers, cookies } from "next/headers";
+import { type NextResponse } from "next/server";
+
+export async function GET(request: NextResponse) {
+  const requestHeaders = new Headers(request.headers);
+  const headerList = headers();
+
+  cookies().set("resultsPerPage", "20");
+  const theme = request.cookies.get("theme");
+
+  console.log(requestHeaders.get("Authorization"));
+  console.log(headerList.get("Authorization"));
+  console.log(cookies().get("resultsPerPage"));
+
+  return new Response("<h1>Profile API data</h>", {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
+    },
+  });
 }
